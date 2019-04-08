224ec034a697a98d832acaa6f07b5aec55aae3ff
marcantoine-g <marc-antoine.g@outlook.com>
2019-04-08 11:39:33 +0200

    serialisation plutot que gson

---
+++ Pokedeck_v2/src/pokedeck_v2/app/Carte_Energie.java
@@ -0,0 +1,9 @@
+package app;
+public class Carte_Energie extends Carte {
+
+    public Carte_Energie(String type, int numero, String extension){
+        super(type, "Energie", numero, extension);
+    }
+
+
+}
